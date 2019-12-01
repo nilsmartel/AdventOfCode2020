07a8a7551c23e130b9f9e2f7602df4e16f528397
Nils Martel <nils.martel@graphmasters.net>
2019-12-01 17:31:49 +0100

    Write Tests, Fix Errors

--- aoc01b/src/main.rs
+++ aoc01b/src/main.rs
@@ -14,7 +14,7 @@ fn get_fuel_for_mass(mass: i64) -> i64 {
     match mass {
         x if x <= 0 => 0,
         mass => {
-            let fuel_mass = mass / 3 - 2;
+            let fuel_mass = (mass / 3 - 2).max(0);
 
             fuel_mass + get_fuel_for_mass(fuel_mass)
         }
@@ -26,12 +26,17 @@ mod test {
     use super::get_fuel_for_mass;
 
     #[test]
-    fn first() {
+    fn example_01() {
         assert_eq!(get_fuel_for_mass(14), 2);
     }
 
     #[test]
-    fn second() {
-        assert_eq!(get_fuel_for_mass(14), 2);
+    fn example_02() {
+        assert_eq!(get_fuel_for_mass(1969), 966);
+    }
+
+    #[test]
+    fn example_03() {
+        assert_eq!(get_fuel_for_mass(100756), 50346);
     }
 }
