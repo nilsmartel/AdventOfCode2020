eff41ba3c1284f1e7e21e0b12d22cace9f50066c
Nils Martel <nils.martel@graphmasters.net>
2019-12-02 08:52:32 +0100

    Start implementation of opcode machine

--- aoc02/src/main.rs
+++ aoc02/src/main.rs
@@ -1,5 +1,24 @@
 fn main() {
-    dbg!(get_codes());
+    let mut i = 0;
+
+    let mut code = get_codes();
+
+    loop {
+        match code[i] {
+            1 => {
+                code[i + 3] = code[i + 1] + code[i + 2];
+            }
+            2 => {
+                code[i + 3] = code[i + 1] * code[i + 2];
+            }
+            99 => break,
+            x => eprintln!("Illegal opcode {}", x),
+        }
+
+        i += 4;
+    }
+
+    println!("{:?}", code);
 }
 
 fn get_codes() -> Vec<usize> {
