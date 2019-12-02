bffc5cab324baa6e7f5699f936bf3804bf28902a
Nils Martel <nils.martel@graphmasters.net>
2019-12-02 08:46:42 +0100

    implement Parsing

--- aoc02/src/main.rs
+++ aoc02/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    dbg!(get_codes());
+}
+
+fn get_codes() -> Vec<usize> {
+    std::fs::read_to_string("input/input.txt")
+        .unwrap()
+        .split(",")
+        .map(|s| s.trim().parse::<usize>().unwrap())
+        .collect()
 }
